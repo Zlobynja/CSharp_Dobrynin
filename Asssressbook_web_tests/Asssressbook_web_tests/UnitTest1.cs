3b13a19964dffee1f24a86a6fe79e4f898684559
zlobynja <zlobynjanikktich@gmail.com>
2019-05-19 02:53:46 +0300

    Задание №2: Создать первый работающий тест

--- Asssressbook_web_tests/Asssressbook_web_tests/UnitTest1.cs
+++ Asssressbook_web_tests/Asssressbook_web_tests/UnitTest1.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Asssressbook_web_tests
+namespace Addressbook_web_tests
 {
     [TestClass]
     public class UnitTest1
