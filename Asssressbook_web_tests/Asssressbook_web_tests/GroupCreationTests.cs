4675b7ea7611f253c4f8cb9125b0b7dc1e51e373
zlobynja <zlobynjanikktich@gmail.com>
2019-06-23 14:14:40 +0300

    Задание №2 (исправленые имена)

--- Asssressbook_web_tests/Asssressbook_web_tests/GroupCreationTests.cs
+++ Asssressbook_web_tests/Asssressbook_web_tests/GroupCreationTests.cs
@@ -7,10 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace Addressbook_web_tests
+namespace WebAddressbookTests
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class GroupCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,7 +40,7 @@ namespace Addressbook_web_tests
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void GroupCreationTest()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Click();
